8eb48214463751413652017a676be2f87a73c749
Abanti Ranadhir Sahasransu <a.r.sahasransu@cern.ch>
2024-04-18 18:54:01 +0200

    Update Elog for shadow shift.

--- TrainingShift_2Shadowing_GenerateElog.py
+++ TrainingShift_2Shadowing_GenerateElog.py
@@ -12,7 +12,18 @@ with open("24_04_17_TRGShifter_Elog.txt", "w") as f:
     ptshiftsum.add_row(['09:23', '09:25', '379543', 'Cosmics. CSC only.'])
     ptshiftsum.add_row(['09:27', '09:41', '379544', 'Cosmics. CSC only. No TWINMUX.'])
     ptshiftsum.add_row(['09:44', '10:20', '379546', 'Cosmics. No TWINMUX, EMTF.'])
-    ptshiftsum.add_row(['10:22', '10:54', '379548', 'Cosmics. No TWINMUX, EMTF.'])
+    ptshiftsum.add_row(['10:22', '10:54', '379548', 'Cosmics. No TWINMUX, EMTF. Systems to local.'])
+    ptshiftsum.add_row(['13:09', '13:21', '379572', 'Systems to global. Cosmics. With ECAL, DT, GEM. No EMTF.'])
+    ptshiftsum.add_row(['13:24', '13:30', '379574', 'Cosmics. No RPC, CSC. No EMTF.'])
+    ptshiftsum.add_row(['13:30', '13:41', '379575', 'Cosmics. No RPC. No EMTF.'])
+    ptshiftsum.add_row(['13:43', '13:49', '379576', 'Start testing new TRG keys. Cosmics. All. No T0 output.'])
+    ptshiftsum.add_row(['13:59', '14:01', '379580', 'Circulating. All. No T0 output. DAQ fail. '])
+    ptshiftsum.add_row(['14:02', '14:04', '379581', 'Circulating. All. No T0 output. DAQ fail.'])
+    ptshiftsum.add_row(['14:07', '14:11', '379582', 'Circulating. All. No T0 output.'])
+    ptshiftsum.add_row(['14:14', '14:20', '379583', 'Collisions. All. No T0 output.'])
+    ptshiftsum.add_row(['14:22', '14:27', '379584', 'LumiScan. All. No T0 output.'])
+    ptshiftsum.add_row(['14:22', '14:53', '379585', 'Cosmics. All except CaloL1, CaloL2.'])
+    ptshiftsum.align['Summary'] = 'l'
     f.write(ptshiftsum.get_string() + '\n\n\n')
 
     f.write(TTAdd('06:54', "Morning shift started."))
@@ -134,7 +145,8 @@ with open("24_04_17_TRGShifter_Elog.txt", "w") as f:
                     'all'
     ))
     f.write(TAdd('\n', 1))
-    f.write(TAdd('No data recorded.', 1))
+    f.write(TAdd('Run ended with DAQ error.', 1))
+    f.write(TAdd('Too short for rate measurement.', 1))
 
     f.write(ER('09:25', 379543))
     # ====================================
@@ -180,8 +192,164 @@ with open("24_04_17_TRGShifter_Elog.txt", "w") as f:
 
     f.write('\n')
     f.write(TTAdd('11:15', "Trigger shift system RAM capacity increased from 8 GB to 16 GB."))
+    f.write(TTAdd('13:00', "Local systems are changed to Global to begin system-wide checks."))
 
-    f.write(TAdd('Comment bla.', 1))
-    f.write(TAdd('Maybe summarise the day plans.', 1))
-    f.write(TAdd('Any major problems faced and if anything specific to look forward to.', 1))
-    f.write(TTAdd('XX:XX', "Morning shift ended."))
+    # ====================================
+    f.write(BR('13:09', 379572))
+    f.write(RunSAdd('l1_hlt_cosmics2024/v76',
+                    '4 - HighRandom',
+                    'Proton Physics: No Beam',
+                    'ECAL, DT and GEM only',
+                    'all expect EMTF'
+    ))
+    f.write(RateSAdd('117 ', '113.8 k', '114 k', 5.1, '125 ', '125 '))
+
+    f.write(ER('13:21', 379572))
+    # ====================================
+
+    # ====================================
+    f.write(BR('13:24', 379574))
+    f.write(RunSAdd('l1_hlt_cosmics2024/v76',
+                    '4 - HighRandom',
+                    'Proton Physics: No Beam',
+                    'all except RPC and CSC',
+                    'all expect EMTF'
+    ))
+    f.write(RateSAdd('118 ', '113.8 k', '114 k', 5.1, '131 ', '125 '))
+
+    f.write(ER('13:30', 379574))
+    # ====================================
+
+    # ====================================
+    f.write(BR('13:30', 379575))
+    f.write(RunSAdd('l1_hlt_cosmics2024/v76',
+                    '4 - HighRandom',
+                    'Proton Physics: No Beam',
+                    'all except RPC',
+                    'all expect EMTF'
+    ))
+    f.write(RateSAdd('119 ', '113.8 k', '114 k', 5.1, '130 ', '125 '))
+
+    f.write(ER('13:41', 379575))
+    # ====================================
+
+    f.write('\n')
+    f.write(TTAdd('13:40', "Obtained the new trigger keys. Test the system with RPC in first before testing the new trigger keys."))
+
+    # ====================================
+    f.write(BR('13:43', 379576))
+    f.write(RunSAdd('l1_hlt_cosmics2024/v77',
+                    '4 - HighRandom',
+                    'Proton Physics: No Beam',
+                    'all',
+                    'all'
+    ))
+    f.write(RateSAdd('133 ', '113.8 k', '114 k', 5.1, '140 ', '131 '))
+    f.write(TAdd('\n', 1))
+    f.write(TAdd('No T0 output', 1))
+
+    f.write(ER('13:49', 379576))
+    # ====================================
+
+    # ====================================
+    f.write(BR('13:59', 379580))
+    f.write(RunSAdd('l1_hlt_circulating2024/v49',
+                    '11 - VHighBPTX',
+                    'Proton Physics: No Beam',
+                    'all',
+                    'all'
+    ))
+    f.write(TAdd('\n', 1))
+    f.write(TAdd('Run ended with DAQ error.', 1))
+    f.write(TAdd('Too short for rate measurement.', 1))
+
+
+    f.write(ER('14:01', 379580))
+    # ====================================
+
+    f.write('\n')
+    f.write(TTAdd('14:00', "LHC proton beam status changed to cycling."))
+
+    # ====================================
+    f.write(BR('14:02', 379581))
+    f.write(RunSAdd('l1_hlt_circulating2024/v49',
+                    '11 - VHighBPTX',
+                    'Proton Physics: Cycling',
+                    'all',
+                    'all'
+    ))
+    f.write(TAdd('\n', 1))
+    f.write(TAdd('Run ended with DAQ error.', 1))
+    f.write(TAdd('Too short for rate measurement.', 1))
+
+
+    f.write(ER('14:04', 379581))
+    # ====================================
+
+    # ====================================
+    f.write(BR('14:06', 379582))
+    f.write(RunSAdd('l1_hlt_circulating2024/v49',
+                    '11 - VHighBPTX',
+                    'Proton Physics: Cycling',
+                    'all',
+                    'all'
+    ))
+    f.write(RateSAdd('282 ', '400 ', '783 ', 1.23, '289 ', '289 '))
+    f.write(TAdd('\n', 1))
+    f.write(TAdd('No T0 output', 1))
+
+
+    f.write(ER('14:11', 379582))
+    # ====================================
+
+    # ====================================
+    f.write(BR('14:14', 379583))
+    f.write(RunSAdd('l1_hlt_collisions2024/v72',
+                    '7 - 2pE034',
+                    'Proton Physics: Cycling',
+                    'all',
+                    'all'
+    ))
+    f.write(RateSAdd('309 ', '400 ', '809 ', 1.23, '36 ', '409 '))
+    f.write(TAdd('\n', 1))
+    f.write(TAdd('No T0 output', 1))
+
+
+    f.write(ER('14:20', 379583))
+    # ====================================
+
+    # ====================================
+    f.write(BR('14:22', 379584))
+    f.write(RunSAdd('l1_hlt_lumiscan/v10',
+                    '4 - L1H6',
+                    'Proton Physics: Cycling',
+                    'all',
+                    'all'
+    ))
+    f.write(RateSAdd('0 ', '397 ', '497 ', 1.25, '0.2 ', '4 '))
+    f.write(TAdd('\n', 1))
+    f.write(TAdd('No T0 output', 1))
+
+
+    f.write(ER('14:27', 379584))
+    # ====================================
+
+    # ====================================
+    f.write(BR('14:22', 379585))
+    f.write(RunSAdd('l1_hlt_cosmics2024/v77',
+                    '4 - HighRandom',
+                    'Proton Physics: Cycling',
+                    'all',
+                    'all except CaloL1 and CaloL2'
+    ))
+    f.write(RateSAdd('120 ', '113.8 k', '114 k', 5.1, '132 ', '132 '))
+
+
+    f.write(ER('14:53', 379585))
+    # ====================================
+
+    f.write(TAdd('Systems taken to local. All except TRG systems Calo L1 and Calo L2 put back.', 1))
+    f.write(TAdd('New TRG keys tested. Maybe expect more TRG keys.', 1))
+    f.write(TAdd('Proton beams cycling. Expect collisions later today.', 1))
+    f.write(TAdd('All TRG systems functioned nominal. Rates were as expected.', 1))
+    f.write(TTAdd('14:56', "Morning shift ended."))
